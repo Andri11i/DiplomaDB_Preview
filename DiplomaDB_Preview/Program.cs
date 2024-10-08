507390fb72b2bbce141e31df60921f88152b1fea
maksb <maksb@MAKSPC>
2024-10-08 18:17:47 +0300

    trying to understand

--- DiplomaDB_Preview/Program.cs
+++ DiplomaDB_Preview/Program.cs
@@ -1,6 +1,16 @@
-﻿class Program
+﻿using DiplomaDB_Preview.Data;
+
+class Program
 {
     static void Main()
     {
+        //using (DiplomaDbPreviewContext context = new DiplomaDbPreviewContext())
+        //{
+        //    foreach (var item in context.Users)
+        //    {
+                
+        //    }
+
+        //}
     }
 }
